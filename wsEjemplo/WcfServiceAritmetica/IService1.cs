ec428cd200dd516dc2326c596ced0039a35c09f9
Paulo Cesar Loayza <pcloayza@gmail.com>
2019-02-22 22:44:36 -0400

    se creo sumar

--- wsEjemplo/WcfServiceAritmetica/IService1.cs
+++ wsEjemplo/WcfServiceAritmetica/IService1.cs
@@ -10,38 +10,31 @@ namespace WcfServiceAritmetica
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IWSaritmetica
     {
-
         [OperationContract]
-        string GetData(int value);
-
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        // TODO: Add your service operations here
+        clsAritmetica sumar(int numero1, int numbero2);
     }
 
-
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
     [DataContract]
-    public class CompositeType
+    public class clsAritmetica:BaseRespuesta
     {
-        bool boolValue = true;
-        string stringValue = "Hello ";
+        [DataMember]
+        public int numeroA { get; set; }
 
         [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
+        public int numeroB { get; set; }
 
         [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
+        public int resultado { get; set; }
+
+    }
+
+    [DataContract]
+    public class BaseRespuesta
+    {
+        public string Mensaje { get; set; }
+        public string Error { get; set; }
+
     }
 }
--- wsEjemplo/WcfServiceAritmetica/Service1.svc.cs
+++ wsEjemplo/WcfServiceAritmetica/Service1.svc.cs
@@ -10,24 +10,18 @@ namespace WcfServiceAritmetica
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    public class WSaritmetica : IWSaritmetica
     {
-        public string GetData(int value)
+        public clsAritmetica sumar(int numero1, int numero2)
         {
-            return string.Format("You entered: {0}", value);
-        }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            clsAritmetica obj = new clsAritmetica();
+            obj.numeroA = numero1;
+            obj.numeroB = numero2;
+            obj.resultado = obj.numeroA + obj.numeroB;
+            obj.Mensaje = "Suma Satisfactoria";
+            if (obj.resultado < 0)
+                obj.Error = "Resultado Negativo";
+            return obj;
         }
     }
 }
